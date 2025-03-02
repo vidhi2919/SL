30e391b60fca64110a520b756ee245b84282a7cf
swastidesisascendeducare <swasti.mishra@desisascendeducare.in>
2025-03-02 14:56:17 +0530

    added lottie-react for animations

--- frontend/src/components/Background-Animation.jsx
+++ frontend/src/components/Background-Animation.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Lottie from "lottie-react";
-import animationData from "../assets/bg1.json"; 
+import animationData from "../assets/bg1.json";
 
 const BackgroundAnimation = () => {
   return (
-    <div className="absolute inset-0 w-full h-full -z-10">
-      <Lottie animationData={animationData} className="w-full h-full object-cover" />
+    <div className="absolute inset-0 w-full h-full -z-10" aria-hidden="true">
+      <Lottie 
+        animationData={animationData} 
+        className="w-full h-full object-cover" 
+        lazyLoad={true}
+      />
     </div>
   );
 };
